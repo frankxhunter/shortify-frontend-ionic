7988cdde44be68dd53dac730b9ee44050e1442fd
frankxhunter <frankxhunter@gmail.com>
2026-04-18 13:29:57 +0200

    Adapting toast copy

--- src/app/components/buttons/copy-button/copy-button/copy-button.component.ts
+++ src/app/components/buttons/copy-button/copy-button/copy-button.component.ts
@@ -1,16 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Clipboard } from '@capacitor/clipboard';
-import { IonCard, IonCardContent, IonIcon, IonButton } from "@ionic/angular/standalone";
+import { Capacitor } from '@capacitor/core';
+import { IonIcon, IonButton, IonToast, ToastController } from "@ionic/angular/standalone";
 import { TranslatePipe } from 'src/app/pipes/translate.pipe';
+import { I18nService } from 'src/app/services/i18n/i18n.service';
 
 @Component({
   selector: 'app-copy-button',
   templateUrl: './copy-button.component.html',
-  imports: [TranslatePipe, IonButton, IonIcon],
+  imports: [TranslatePipe, IonButton, IonIcon, IonToast],
   styleUrls: ['./copy-button.component.scss'],
 })
 export class CopyButtonComponent {
   @Input() text: string = '';
+
+  toastController = inject(ToastController)
+  i18nService = inject(I18nService);
+  toast: HTMLIonToastElement | null = null
 
   isCopied = false;
   private timeout: any;
@@ -23,15 +29,37 @@ export class CopyButtonComponent {
 
     try {
       await Clipboard.write({ string: this.text });
+      if (Capacitor.getPlatform() === 'web') {
+        this.copyToast()
+      }
       this.isCopied = true;
       console.log('Copied')
 
       clearTimeout(this.timeout);
       this.timeout = setTimeout(() => {
         this.isCopied = false;
-      }, 800);
+      }, 1700);
     } catch (err) {
       console.error('Error al copiar:', err);
     }
+  }
+
+  async copyToast() {
+    if (this.toast) {
+      await this.toast.dismiss();
+      this.toast = null;
+    }
+
+    this.toast = await this.toastController.create({
+      message: this.i18nService.translate('home.copied'),
+      duration: 1200,
+      position: 'bottom',
+      icon: 'checkmark-circle-outline',
+      cssClass: 'custom-toast',
+      swipeGesture: "vertical",
+      positionAnchor: "middle"
+    });
+
+    await this.toast.present();
   }
 }
