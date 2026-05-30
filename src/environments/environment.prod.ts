0502b4794f8dbe38554ee558bcb9c77c077ff246
frankxhunter <frankxhunter@gmail.com>
2026-05-30 12:50:43 +0200

    Update production environment settings for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://192.168.1.52:8080',
+  production: true,
+  apiUrl: 'https://shortfy.link/',
   wsUrl: 'ws://localhost:8080/ws',
   googleClientId: "435102139040-7g3peavcksvsajr0hpmtpuavbvqldh5q.apps.googleusercontent.com",
-  googleAndroidClientId: "435102139040-3u1prgc0bc9u7g3iggqfg3nlfmu7mm3s.apps.googleusercontent.com"
+  googleAndroidClientId: "435102139040-3u1prgc0bc9u7g3iggqfg3nlfmu7mm3s.apps.googleusercontent.com",
+  useMockAnalytics: false,
 };
