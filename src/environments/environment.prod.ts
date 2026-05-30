adab444e39b4274beeb7427968f970f3aabcf561
frankxhunter <frankxhunter@gmail.com>
2026-05-30 12:56:37 +0200

    Update WebSocket URL to use secure connection

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://shortfy.link/',
-  wsUrl: 'ws://localhost:8080/ws',
+  wsUrl: 'wss://shortfy.link/ws',
   googleClientId: "435102139040-7g3peavcksvsajr0hpmtpuavbvqldh5q.apps.googleusercontent.com",
   googleAndroidClientId: "435102139040-3u1prgc0bc9u7g3iggqfg3nlfmu7mm3s.apps.googleusercontent.com",
   useMockAnalytics: false,
