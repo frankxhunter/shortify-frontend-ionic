96be68c2a3e2061b52b3fa4506f4a9a22fce19bc
frankxhunter <frankxhunter@gmail.com>
2026-05-30 13:03:37 +0200

    Remove trailing slash from apiUrl in production environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://shortfy.link/',
+  apiUrl: 'https://shortfy.link',
   wsUrl: 'wss://shortfy.link/ws',
   googleClientId: "435102139040-7g3peavcksvsajr0hpmtpuavbvqldh5q.apps.googleusercontent.com",
   googleAndroidClientId: "435102139040-3u1prgc0bc9u7g3iggqfg3nlfmu7mm3s.apps.googleusercontent.com",
